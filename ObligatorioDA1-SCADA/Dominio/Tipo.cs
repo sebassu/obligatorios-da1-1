42cdfcfe107f54b05f068ca2abd6b7524a57b1cd
Juan Rodríguez <juanca_el1@hotmail.es>
2016-04-23 16:51:47 -0300

    [PruebasExistosas] [Refactoring]: Codificación de los métodos asociados a la operación de set de nombre de la clase Tipo

---
+++ ObligatorioDA1-SCADA/Dominio/Tipo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Dominio
+{
+    public class Tipo
+    {
+        private string nombre;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value) && contieneCaracteresAlfabeticos(value))
+                {
+                    this.nombre = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        private bool contieneCaracteresAlfabeticos(string unValor)
+        {
+            Regex caracteresAlfabeticos = new Regex(@"[A-Z]", RegexOptions.IgnoreCase);
+            return caracteresAlfabeticos.IsMatch(unValor);
+        }
+    }
+}
