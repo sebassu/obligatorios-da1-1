a57340eae8ddbb81c61bd2e6b4d91212de9a671b
Juan Rodríguez <juanca_el1@hotmail.es>
2016-04-23 16:36:02 -0300

    [NoCompila]: Se generaron las pruebas de set del nombre de la clase Tipo

---
+++ ObligatorioDA1-SCADA/UnitTestProject1/TipoTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TipoTest
+    {
+        [TestMethod]
+        public void setNombreTest1()
+        {
+            Tipo unTipo = new Tipo();
+            unTipo.Nombre = "Eléctrico";
+            Assert.AreEqual("Eléctrico", unTipo.Nombre);
+        }
+
+        [TestMethod]
+        public void setNombreTest2()
+        {
+            Tipo unTipo = new Tipo();
+            unTipo.Nombre = "  Modelo AX-453  ";
+            Assert.AreEqual("Modelo AX-453", unTipo.Nombre);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void setNombreTest3()
+        {
+            Tipo unTipo = new Tipo();
+            unTipo.Nombre = "1234";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void setNombreTest4()
+        {
+            Tipo unTipo = new Tipo();
+            unTipo.Nombre = "!@.$#%   *-/";
+        }
+    }
+}
