44c9426d8d34fd5d46487ddae9a4ebc3acbdbf68
Juan Rodríguez <juanca_el1@hotmail.es>
2016-04-24 16:44:03 -0300

    [NoCompila]: Se generaron pruebas de set de Tipo de un Dispositivo

--- ObligatorioDA1-SCADA/PruebasUnitarias/DispositivoTest.cs
+++ ObligatorioDA1-SCADA/PruebasUnitarias/DispositivoTest.cs
@@ -62,5 +62,41 @@ namespace PruebasUnitarias
             unDispositivo.EnUso = false;
             Assert.AreEqual(false, unDispositivo.EnUso);
         }
+
+        [TestMethod]
+        public void SetDispositivoTipoTest1()
+        {
+            Dispositivo unDispositivo = new Dispositivo();
+            Tipo unTipo = new Tipo();
+            unDispositivo.Tipo = unTipo;
+            Assert.AreEqual(unTipo, unDispositivo.Tipo);
+        }
+
+        [TestMethod]
+        public void SetDispositivoTipoTest2()
+        {
+            Dispositivo unDispositivo = new Dispositivo();
+            Tipo unTipo = Tipo.ConNombreDescripcion("Eléctrico", "Es muy bueno");
+            unDispositivo.Tipo = unTipo;
+            Assert.AreEqual(unTipo, unDispositivo.Tipo);
+        }
+
+        [TestMethod]
+        public void SetDispositivoTipoTest3()
+        {
+            Dispositivo unDispositivo = new Dispositivo();
+            Tipo unTipo = Tipo.ConNombreDescripcion("Eléctrico", "Es muy bueno");
+            Tipo otroTipo = Tipo.ConNombreDescripcion("Eléctrico", "Es muy bueno");
+            unDispositivo.Tipo = unTipo;
+            Assert.AreEqual(otroTipo, unDispositivo.Tipo);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetDispositivoTipoTest4()
+        {
+            Dispositivo unDispositivo = new Dispositivo();
+            unDispositivo.Tipo = null;
+        }
     }
 }
